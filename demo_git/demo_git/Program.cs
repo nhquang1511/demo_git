e527a47b40265687979b7e301f46587d88b65c64
huu quang <huuquang1511@gmail.com>
2024-01-26 20:58:35 +0700

    init project

---
+++ demo_git/demo_git/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Khai báo biến để lưu trữ hai số a và b
+        int a, b;
+
+        a = 5;
+        b = 6;
+  
+        int sum = a + b;
+        Console.WriteLine($"Tổng của {a} và {b} là {sum}");
+
+        // Dừng chương trình để người dùng có thể xem kết quả trước khi đóng cửa sổ console
+        Console.ReadLine();
+    }
+}
